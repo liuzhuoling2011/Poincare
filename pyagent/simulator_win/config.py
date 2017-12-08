267dbc7eb7a8380ceecb2a8196f639d5ea0b318d
liuzhuoling <liuzhuoling@mycapital.net>
2017-12-08 11:16:59 +0800

    remove pyagent

--- pyagent/simulator_win/config.py
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-
-config_json = {
-    "TASK_HANDLER": "main.py",
-    "BASE_PATH": ".\\",
-    "LOG_DIR": "agent_log",
-    "AGENT_LOG_FILE": "%s\\agent_%s.log",
-    "STRAT_CONF": "config\\strat_config.json",
-}
